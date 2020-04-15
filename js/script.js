31723e36de801d1f034725f6eab5b29c167279f3
Boris <48370748+BorisSlonov@users.noreply.github.com>
2020-04-15 13:55:20 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+var typed1 = new Typed('#typed2', {
+    strings: ['Slonov Boris', 'Web Dev', 'from Saint P'],
+    typeSpeed: 80,
+    backSpeed: 50,
+    fadeOut: true,
+    loop: true,
+    startDelay: 8000,
+    cursorChar: '_'
+});
+
+
+var typed2 = new Typed('#typed3', {
+    strings: ['Hello!', 'im Slonov Boris', 'Web Developer'],
+    typeSpeed: 80,
+    backSpeed: 50,
+    fadeOut: false,
+    loop: false,
+    cursorChar: ''
+});
+
+var typed3 = new Typed('#typed4', {
+    strings: ['Have a Nice Day!', 'Just Smile!:)', 'Slonov Boris'],
+    typeSpeed: 80,
+    backSpeed: 50,
+    fadeOut: false,
+    loop: false,
+    cursorChar: ''
+});
