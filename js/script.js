356703610007e74288ed68c4e537215252ffefcd
Boris <sslonovborisss@gmail.com>
2022-01-22 14:40:07 +0300

    add

--- js/script.js
+++ js/script.js
@@ -1,58 +1,65 @@
-window.onload = function TypeText() {
-
-    new Typed('#typed2', {
-        strings: ['Slonov Boris'],
-        typeSpeed: 80,
-        backSpeed: 20,
-        fadeOut: true,
-        loop: false,
-        startDelay: 7600,
-        cursorChar: '_'
-    });
+if (!sessionStorage.alreadyClicked) {
 
 
+    window.onload = function TypeText() {
 
-    new Typed('#typed3', {
-        strings: ['_', ' ', 'Hello!', 'Im Slonov Boris', 'Web Developer'],
-        typeSpeed: 30,
-        backSpeed: 25,
-        fadeOut: false,
-        loop: false,
-        cursorChar: ''
-    });
-
-
-
-
-
-    $(document).ready(function () {
-        setInterval(() => {
+        new Typed('#typed2', {
+            strings: ['Boris Slonov'],
+            typeSpeed: 80,
+            backSpeed: 20,
+            fadeOut: true,
+            loop: false,
+            startDelay: 7600,
+            cursorChar: '_'
+        });
+    
+    
+    
+        new Typed('#typed3', {
+            strings: ['_', ' ', 'Hello!', 'Im Boris Slonov', 'Web Developer'],
+            typeSpeed: 30,
+            backSpeed: 25,
+            fadeOut: false,
+            loop: false,
+            cursorChar: ''
+        });
+    
+    
+    
+    
+        $(document).ready(function () {
             setInterval(() => {
-                $('.vis').fadeIn(500)
-            }, 4250);
+                setInterval(() => {
+                    $('.vis').fadeIn(500)
+                }, 4250);
+    
+                setInterval(() => {
+                    $('.vis-btn').fadeIn(500)
+                }, 4750);
+    
+                setInterval(() => {
+                    $('.vis-content').fadeIn(100)
+                }, 5000);
+    
+                setInterval(() => {
+                    $('.vis-header').fadeIn(100);
+                }, 5000);
+    
+    
+                setInterval(() => {
+                    $('#h100vh').fadeOut(0);
+    
+                }, 5000);
+            }, 1150);
+        });
+    
+        new WOW().init();
+    };
 
-            setInterval(() => {
-                $('.vis-btn').fadeIn(500)
-            }, 4750);
 
-            setInterval(() => {
-                $('.vis-content').fadeIn(100)
-            }, 5000);
-
-            setInterval(() => {
-                $('.vis-header').fadeIn(100);
-            }, 5000);
+}
 
 
-            setInterval(() => {
-                $('#h100vh').fadeOut(0);
-
-            }, 5000);
-        }, 1150);
-    });
-
-    new WOW().init();
-};
 
 
 
